617684143e85eaa39c8ee14c02fbb49c3f7f4382
MohammadFarzan <farzanmohammad80@gmail.com>
2021-11-07 23:56:12 +0330

    phase 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,96 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+
 public class Main {
+
+    static int cursor = 0;
+
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        ArrayList<String> tokens = new ArrayList<>();
+        while (scanner.hasNext()) {
+            cursor = 0;
+            s = scanner.nextLine();
+            char[] line = s.toCharArray();
+            if (s.length() == 0)
+                continue;
+
+
+            String lastWord = "";
+            while (cursor < line.length) {
+                String word = readNextWord(line);
+
+                if (Tools.isKeyword(word)) {
+                    System.out.println("keyword:: " + word);
+                } else if (Tools.isOperator(word)) {
+                    System.out.println("operator:: " + word);
+                }else if (Tools.isNumber(word)){
+                    System.out.println("number:: " + word);
+                }
+                else if(!Tools.isWhiteSpace(word)) {
+                    if (Tools.isSymbol(word))
+                        System.out.println("identifier:: " + Tools.getSymbol(word));
+                    else {
+                        System.out.println("identifier:: " + lastWord);
+                        Tools.addToSymbols(word, lastWord);
+                    }
+                }
+                lastWord = word;
+
+            }
+
+        }
+
     }
+
+    static String readNextWord(char[] line) {
+        StringBuilder tempStr = new StringBuilder();
+        while (cursor < line.length && !Tools.isWhiteSpace(line[cursor])) {
+
+            if (Tools.isOperator(line[cursor])){
+                if (tempStr.toString().isEmpty()){
+                    tempStr.append(line[cursor]);
+                    cursor++;
+                    if (cursor >= line.length) {
+                        return tempStr.toString();
+                    }
+
+                    String temp2 = tempStr.toString() + line[cursor];
+
+                    if (Tools.isOperator(temp2)) {
+                        cursor++;
+                        return temp2;
+                    }
+                    else {
+                        return tempStr.toString();
+                    }
+
+                }
+                else
+                    return tempStr.toString();
+            }else {
+
+                tempStr.append(line[cursor]);
+                cursor++;
+            }
+        }
+        cursor++;
+        return tempStr.toString();
+    }
+
 }
+/*
+int main(){
+int a=3;
+float b=5;
+int c=7;
+int d=a+c;
+for(int i=0;i<3;i++){
+}
+
+}
+ */
--- src/Tools.java
+++ src/Tools.java
@@ -1,2 +1,52 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Tools {
+    private static final HashSet<String> keyWords = new HashSet<>(Arrays.asList(
+            "main" ,
+            "auto","break","case","char","const","continue","default",
+            "do","double","else","enum","extern","float","for","goto",
+            "if","int","long","register","return","short","signed",
+            "sizeof","static","struct","switch","typedef","union",
+            "unsigned","void","volatile","while"));
+    private static final HashSet<String> operator = new HashSet<>(Arrays.asList("(",")","{","}","[","]",
+            ";","=","==","!=","+","-","++","--","<",">","<=",">=","&","&&","|","||"));
+
+    private static final HashMap<String , String> symbolTable = new HashMap<>();
+
+    public static boolean isKeyword(String word){
+        return keyWords.contains(word);
+    }
+    public static boolean isOperator(String opt){
+        return operator.contains(opt);
+    }
+    public static boolean isOperator(char opt){
+        return isOperator(String.valueOf(opt));
+    }
+    public static boolean isNumber(String n){
+        if (n.isEmpty())
+            return false;
+        char num = n.charAt(0);
+        return num >= '0' && num <= '9';
+    }
+
+    public static boolean isWhiteSpace(String s){
+        return s.matches("^\\s*$");
+    }
+    public static boolean isWhiteSpace(char c){
+        String s = String.valueOf(c);
+        return isWhiteSpace(s);
+    }
+
+    public static void addToSymbols(String name , String type){
+        symbolTable.put(name, type);
+    }
+    public static String getSymbol(String name){
+        return symbolTable.get(name);
+    }
+    public static boolean isSymbol(String name){
+        return symbolTable.containsKey(name);
+    }
+
 }
